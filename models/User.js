fcdfd69d23b80cf4bb04c8b579db6b6f2b3ea179
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-03-22 13:24:29 +0200

    [Chore #164489773] resolve .env models/User.js package.json package-lock.json

--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema(
       lowercase: true,
       unique: true,
       required: [true, "can't be blank"],
-      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
       index: true
     },
     email: {
