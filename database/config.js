de476c9a02c7e6f55f602abbe8788f5ff38d2d9f
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-03-26 10:37:42 +0200

    ft-164489772 configuring sequelize

---
+++ database/config.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let dbname = (process.env.NODE_ENV === 'test') ? process.env.TESTDBNAME : process.env.DBNAME;
+
+const sequelize = new Sequelize(dbname,process.env.DBUSERNAME,process.env.DBPASSWORD,{
+  host: process.env.DBHOST,
+  dialect: 'postgres',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+});
+
+export default sequelize;
