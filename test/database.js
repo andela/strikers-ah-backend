55fbd241524861817adb8fa32bb6d8e9b1b4cd94
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-03-26 10:40:13 +0200

    ft-164489772 testing database connection

---
+++ test/database.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+import dotenv from 'dotenv';
+dotenv.config();
+import sequelize from '../database/config';
+
+process.env.NODE_ENV ='test';
+
+chai.should();
+describe('Test database connection', () => {
+  it('should start the database engine' ,(done) => {
+    sequelize.should.be.a('object');
+    sequelize.config.should.have.property('username').eql(process.env.DBUSERNAME);
+    sequelize.config.should.have.property('database').eql(process.env.TESTDBNAME);
+    done();
+  });
+});
