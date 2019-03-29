f30154af2d3a533e11647c99d97c502041ad2084
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2019-03-29 15:49:42 +0200

    chore(swagger): Setup API documentation using swagger Installing swagger, yamljs, swagger-ui-express
    Adding .prettier for es6 proper single quotes configurations
    
    Removing underscore
    
    [Delivers #164489778]

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import routes from './routes/routes';
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', routes);
 
 const port = process.env.PORT || 3000;
