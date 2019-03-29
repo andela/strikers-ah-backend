340f6254f404564ec6a898980edf866fd5f78e9d
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-03-29 11:53:34 +0200

    [Chore 164489736] add a home route for sending a status

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use('/', (req, res) => {
+  res.sendStatus(200);
+});
 
+app.use('/api', routes);
 const port = process.env.PORT || 3000;
 
 app.listen(port);
