ec7859dad2608c0c7692605c5ee6f93a084d8371
mwibutsa <mflohost@gmail.com>
2019-04-02 18:24:55 +0200

    Feature #164489784 Create use Controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,61 @@
+import uuid from 'uuid';
+import select from 'lodash';
 import userModel from '../models/user';
 import helper from '../helpers/helper';
+
 /**
  * @param { class } User -- User }
  */
 class User {
   /**
+   * @author: Mwibutsa Floribert
    * @param {Object} req -- request object
    * @param {Object} res  -- response object
-   * @returns { null } -- returns nothing
-   * @param {String} author -- Mwibutsa Floribert
+   * @returns { Middleware } -- returns nothing
    */
-  async signUpWithEmail(req, res) {
-    this.req = req;
-    this.res = res;
+  static async signUpWithEmail(req, res) {
     // get request values
     const {
-      firsname, lastname, email, password, username
-    } = this.req.body;
+      firstname, lastname, email, password, username
+    } = req.body;
+    let insertedUser;
+    let token;
     // register user in the database
-    this.user = await userModel.create({
-      firsname, lastname, email, password: await helper.hashPassword(password), username
-    });
-    return res.status(201).json({
+    try {
+      const newUser = {
+        id: uuid.v4(),
+        firstname,
+        lastname,
+        email,
+        password: await helper.hashPassword(password),
+        username
+      };
+      // check if the user does not already exist
+      const emailUsed = await userModel.findOne({ where: { email: newUser.email } });
+      const userNameUsed = await userModel.findOne({ where: { username: newUser.username } });
+      if (emailUsed && userNameUsed) {
+        return res.status(400).json({ error: 'Email and username are already in use' });
+      }
+      if (emailUsed) {
+        return res.status(400).json({ error: 'The Email is already in use' });
+      }
+      if (userNameUsed) {
+        return res.status(400).json({ error: 'The username is not available' });
+      }
+      insertedUser = await userModel.create(newUser);
+      token = helper.generateToken(select.pick(insertedUser, ['username', 'email', 'firstname', 'lastname']));
+    } catch (error) {
+      return res.status(500).json({ error: `${error}` });
+    }
+    return res.header('x-auth-token', token).status(201).json({
       user: {
-        username: this.user.username,
-        email: this.user.email,
-        password: this.user.password
+        username: insertedUser.username,
+        email: insertedUser.email,
+        password: insertedUser.password
       }
     });
   }
 }
 
-const user = new User();
-export default user;
+
+export default User;
