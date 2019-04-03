9d2e576f40b4031a37e45cc3796017e5d4716007
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-04-03 17:34:47 +0200

    ft 164489784 refactoring user model for code quality checks

--- models/user.js
+++ models/user.js
@@ -1,43 +1,21 @@
 const USerModel = (Sequelize, DataTypes) => {
   const User = Sequelize.define('user', {
-    firstname: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    bio: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    image: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    password: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    provider: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    provideruserid: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    }
-  }, {});
-  return User;
-};
+    name: { type: DataTypes.STRING, allowNull: true },
 
+    username: { type: DataTypes.STRING, allowNull: false },
+
+    email: { type: DataTypes.STRING, allowNull: true },
+
+    bio: { type: DataTypes.STRING, allowNull: true, },
+
+    image: { type: DataTypes.TEXT, allowNull: true },
+
+    password: { type: DataTypes.TEXT, allowNull: true, },
+
+    provider: { type: DataTypes.STRING, allowNull: true, },
+
+    provideruserid: { type: DataTypes.STRING, allowNull: true, }
+
+  }, {}); return User;
+};
 export default USerModel;
