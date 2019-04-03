cb6723d3442424944987f5a96d925087ff7d1472
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-04-03 22:12:00 +0200

    ft 164489784:

--- controllers/user.js
+++ controllers/user.js
@@ -2,5 +2,20 @@
  * @author frank harerimana
  */
 class User {
+  /**
+   * @author frank harerimana
+   * @param {*} req Social user
+   * @param {*} res logged
+   * @returns { object } user logged in
+   */
+  static socialLogin(req, res) {
+    const user = {
+      username: req.user.username,
+      email: req.user.email,
+    };
+    return res.status(200).json({
+      data: user,
+    });
+  }
 }
 export default User;
--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,9 @@ import user from '../controllers/user';
 const route = express.Router();
 
 route.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
-route.get('/google/redirect', passport.authenticate('google', { failureRedirect: 'auth/google' }), user.googleLogin);
+route.get('/google/redirect', passport.authenticate('google', { failureRedirect: 'auth/google' }), user.socialLogin);
 // passport.authenticate('google', user.googleLogin)
 route.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
-route.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }), user.facebookLogin);
+route.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }), user.socialLogin);
 
 export default route;
