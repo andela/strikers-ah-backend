7b6a5a1048be873cf8ec0d250041b4d08da62da6
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-04-05 11:13:49 +0200

    ft 164489784 adding the host environment

--- config/config.js
+++ config/config.js
@@ -7,21 +7,21 @@ module.exports = {
     username: process.env.DBUSERNAME,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
-    host: '127.0.0.1',
+    host: process.env.DBHOST,
     dialect: 'postgres'
   },
   test: {
     username: process.env.DBUSERNAME,
     password: process.env.PDBPASSWORD,
     database: process.env.TESTDBNAME,
-    host: '127.0.0.1',
+    host: process.env.DBHOST,
     dialect: 'postgres'
   },
   production: {
     username: process.env.DBUSERNAME,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME,
-    host: '127.0.0.1',
+    host: process.env.DBHOST,
     dialect: 'postgres'
   }
 };
