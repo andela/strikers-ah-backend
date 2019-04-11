c23e8997b72cee34e5ca7e92de135e1e8095ea9c
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-04-11 12:04:36 +0200

    render html files

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const strategy = new Strategy();
 app.use('/api/v1/login', user);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(express.static(path.resolve(__dirname, 'view/')));
 app.use('/', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '/view/') });
 });
 
 
