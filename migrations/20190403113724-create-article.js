1866cf40fa91444606a8d0e10c41b4f8a539ad45
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2019-04-13 20:53:42 +0200

    Feature(article): Adding routes and migration
    Creates the route in index.js
    Creates routes for article
    Installing useful package
    adding test fot the home route
    Adding moch data to be used in tests
    
    [Finishes #164489786]

--- migrations/20190403113724-create-article.js
+++ migrations/20190403113724-create-article.js
@@ -4,7 +4,11 @@ module.exports = {
     id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
     slug: { type: Sequelize.STRING, allowNull: false, unique: true },
     title: { type: Sequelize.STRING, required: true },
+<<<<<<< HEAD
     description: { type: Sequelize.TEXT, allowNull: false },
+=======
+    description: { type: Sequelize.TEXT, allowNull: true },
+>>>>>>> Feature(article): Adding routes and migration
     body: { type: Sequelize.TEXT, required: true },
     taglist: { type: Sequelize.ARRAY(Sequelize.STRING), defaultValue: [] },
     authorid: { type: Sequelize.INTEGER, allowNull: false },
