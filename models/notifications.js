3b1ec3f8832a463b515d66a519647653c81d64db
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-04-25 10:20:51 +0200

    ft 164489793 notification models configuration

---
+++ models/notifications.js
@@ -0,0 +1,20 @@
+const notificationModel = (sequelize, DataTypes) => {
+  const Notifications = sequelize.define('notifications', {
+    userid: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    message: DataTypes.STRING,
+    link: DataTypes.TEXT
+  }, {});
+  Notifications.associate = (models) => {
+    Notifications.belongsTo(models.user, { foreignKey: 'userid', onDelete: 'CASCADE' });
+  };
+  Notifications.newRecord = (userid, type, message, link) => {
+    Notifications.create({
+      userid, type, message, link
+    });
+  };
+  Notifications.unfollow = id => Notifications.destroy({ where: { id } });
+  return Notifications;
+};
+
+export default notificationModel;
