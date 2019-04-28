b6d9adbb6a5f0679c0d90dd7ca9f056b27f1adfb
Jacques Nyilinkindi <jacknyilinkindi@gmail.com>
2019-04-28 13:43:30 +0200

    Ft 164489790 Added articlecomment model

---
+++ migrations/20190428113359-create-articlecomment.js
@@ -0,0 +1,36 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('articlecomment', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userid: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    articleid: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    comment: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      default: true
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+
+  }),
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => Promise.all([queryInterface.dropTable('articlecomment')])
+};
