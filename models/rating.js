e0b1c1f05f7449b38d726cefbbe2482d27634fbf
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-04-28 17:52:52 +0200

    [Feature rate-article] document this feature with swagger

--- models/rating.js
+++ models/rating.js
@@ -1,12 +1,9 @@
-
 module.exports = (sequelize, DataTypes) => {
   const rating = sequelize.define('rating', {
     userId: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
     articleSlug: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -15,15 +12,6 @@ module.exports = (sequelize, DataTypes) => {
         key: 'slug'
       },
       onDelete: 'CASCADE'
-=======
-    articleId: {
-      type: DataTypes.INTEGER,
-=======
-    articleSlug: {
-      type: DataTypes.STRING,
->>>>>>> get user from token
-      allowNull: false
->>>>>>> add rating models
     },
     rating: {
       type: DataTypes.INTEGER,
@@ -32,8 +20,6 @@ module.exports = (sequelize, DataTypes) => {
 
   }, {});
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   rating.addRate = (rate, article, id) => rating.findOrCreate({
     where: { userId: id, articleSlug: article },
     defaults: { rating: rate }
@@ -72,21 +58,6 @@ module.exports = (sequelize, DataTypes) => {
   rating.associate = (models) => {
     rating.belongsTo(models.user, { foreignKey: 'userId', onDelete: 'CASCADE' });
     rating.belongsTo(models.article, { foreignKey: 'articleSlug', onDelete: 'CASCADE' });
-=======
-  rating.rateCheck = (rate, article, id) => rating.findOrCreate({ where: { userId: id, articleId: article }, defaults: { rating: rate } });
-=======
-  rating.rateCheck = (rate, article, id) => rating.findOrCreate({ where: { userId: id, articleSlug: article }, defaults: { rating: rate } });
->>>>>>> get user from token
-  rating.rateUpdate = (rateId, rate) => rating.update({ rating: rate }, { returning: true, where: { id: rateId } });
-  rating.avgFind = id => rating.findAll({ where: { articleId: id }, attributes: ['articleId', [sequelize.fn('AVG', sequelize.col('rating')), 'avgRating']], group: 'rating.articleId' });
-  rating.associate = (models) => {
-    rating.belongsTo(models.user, { foreignKey: 'userId', onDelete: 'CASCADE' });
-<<<<<<< HEAD
-    rating.belongsTo(models.article, { foreignKey: 'articleId', onDelete: 'CASCADE' });
->>>>>>> add rating models
-=======
-    rating.belongsTo(models.article, { foreignKey: 'articleSlug', onDelete: 'CASCADE' });
->>>>>>> get user from token
   };
 
   return rating;
