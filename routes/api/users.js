21acc2589676fb7381ca6878b6d017d675043e9f
Jacques Nyilinkindi <jacknyilinkindi@gmail.com>
2019-04-29 13:54:58 +0200

    Ft 165594554 Implemented assigning a user a role

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/:username/stats', secureRoute, user.getReadingHistory);
 router.get('/', secureRoute, user.getAllUsers);
 router.get('/:username', secureRoute, user.getUserInformation);
+router.post('/:username/role', secureRoute, user.assignRole);
 
 export default router;
