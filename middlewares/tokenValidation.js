cb555f90bdd309dbc3d5577ea5b568b9c1a54b33
Jacques Nyilinkindi <jacknyilinkindi@gmail.com>
2019-05-15 11:11:24 +0200

    Secured assign role route

--- middlewares/tokenValidation.js
+++ middlewares/tokenValidation.js
@@ -2,8 +2,11 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import promiseResolver from '../helpers/promiseResolver';
 import client from '../config/redisConfig';
+import model from '../models/index';
 
 dotenv.config();
+
+const { user: UserModel } = model;
 
 const validateToken = async (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers.authorization || req.headers['x-auth-token'];
@@ -24,8 +27,14 @@ const validateToken = async (req, res, next) => {
         }
       }
       const { id: userid } = decoded;
-      req.user = userid;
-      next();
+      const user = await UserModel.findUser(userid);
+      if (user) {
+        req.user = user.id;
+        req.userRole = user.role;
+        next();
+      } else {
+        return res.send({ status: 403, error: 'Forbidden Access' });
+      }
     } catch (error) {
       res.status(400).send({ status: 400, message: error });
     }
